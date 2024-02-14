dd2b08278a714a15f438e59f360036ebb0039525
Sergio Schibelbain <75342387+artikusnc@users.noreply.github.com>
2024-02-14 12:08:28 +0100

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucketSergio"
- 
-  tags = {
-    Name        = "Sergios S3 bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "sergio_bucket" {
+  bucket = "my-tf-test-bucketsergio"
 }
